4edf623f34276821ef410c67a66987833c1e7af5
Sema15 <semkovserg05@gmail.com>
2021-03-30 01:12:55 +0300

    add multiple routing

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,14 +1,45 @@
 import React, {Component} from 'react';
-// import {useRouteMatch} from "react-router-dom";
+import {Switch, Route, Link, useRouteMatch, useParams} from "react-router-dom";
 
 class Dashboard extends Component {
   render() {
-    // let match = useRouteMatch();
-
     return (
-        <p>match</p>
+        <div>
+          <Topics/>
+        </div>
     );
   }
+}
+
+function Topics() {
+  let match = useRouteMatch();
+
+  return (
+      <div>
+        <h2>Dashboard</h2>
+        <ul>
+          <li>
+            <Link to={`${match.url}/components`}>Components</Link>
+          </li>
+          <li>
+            <Link to={`${match.url}/props-v-state`}>Props v. State</Link>
+          </li>
+        </ul>
+        <Switch>
+          <Route path={`${match.path}/:topicId`}>
+            <Topic />
+          </Route>
+          <Route path={match.path}>
+            <h3>Please select a topic.</h3>
+          </Route>
+        </Switch>
+      </div>
+  );
+}
+
+function Topic() {
+  let { topicId } = useParams();
+  return <h3>Requested topic ID: {topicId}</h3>;
 }
 
 export default Dashboard;
